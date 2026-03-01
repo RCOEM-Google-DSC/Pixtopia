cf45e4206029cd2cf6acc853ffc2d907f345cda8
SaptanshuWanjari <saptanshuwanjari63@gmail.com>
2026-03-01 22:14:31 +0530

    initial folder structure

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex min-h-screen items-center justify-center flex-col p-10 gap-10"></div>
+		<div className="flex min-h-screen items-center justify-center flex-col p-10 gap-10">
+			<h1>GDG PixTopia</h1>
+		</div>
 	);
 }
-
